ae76f8ec69a16ab8a2f598bf363be6a0889719d4
Estevao Simao <estevaobernardosimao@email.com>
2025-09-29 10:16:30 +0200

    Initial commit for Vercel deploy

---
+++ script.js
@@ -0,0 +1,43 @@
+const apiKey = "5a6372c16792c91b062fd6850a44b117";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        const data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "mist.png";
+        }
+
+        // ✅ only show when city is valid
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
